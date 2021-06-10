8d1fbd8d62327ff80432b4161ff880d2d982e2e6
avmcd <makximenko@gmail.com>
2021-06-09 23:27:21 -0700

    pulling in the whole neighborhood

--- whaleorfailbot/main.py
+++ whaleorfailbot/main.py
@@ -1,16 +1,17 @@
 import json
 import os
-import random
 
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix='wof')
+bot = commands.Bot(command_prefix="wof")
 
-#todo: create json with ratio's
+# todo: create json with ratio's
+
 
 def add_score(member: discord.Member, amount: int):
     if os.path.isfile("file.json"):
@@ -18,19 +19,25 @@ def add_score(member: discord.Member, amount: int):
             data = json.load(fp)
         try:
             data[f"{member.id}"]["score"] += amount
-        except KeyError: # if the user isn't in the file, do the following
-            data[f"{member.id}"] = {"score": amount} # add other things you want to store
+        except KeyError:  # if the user isn't in the file, do the following
+            data[f"{member.id}"] = {
+                "score": amount,
+            }  # add other things you want to store
     else:
         data = {f"{member.id}": {"score": amount}}
     # saving the file outside of the if statements saves us having to write it twice
     with open("file.json", "w+") as fp:
-        json.dump(data, fp, sort_keys=True, indent=4) # kwargs for beautification
-   # you can also return the new/updated score here if you want
+        json.dump(data, fp, sort_keys=True, indent=4)  # kwargs for beautification
+
+
+# you can also return the new/updated score here if you want
+
 
 def get_score(member: discord.Member):
     with open("file.json", "r") as fp:
         data = json.load(fp)
     return data[f"{member.id}"]["score"]
+
 
 @bot.command()
 async def cmd(ctx):
@@ -39,6 +46,7 @@ async def cmd(ctx):
     # 10 is just an example
     # you can use the random module if you want - random.randint(x, y)
     await ctx.send(f"You now have {get_score(ctx.author)} score!")
+
 
 # TODO: Create command that lists out the ratio's i'm using
 
